d9847bec05754a86ef9061aa6dfba9242449fd29
Vitória Evangelista Santos <20211170150037@ifrn.local>
2023-12-06 17:24:43 -0300

    criação

---
+++ jogo_wendell/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public int vida;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+    }
+}
