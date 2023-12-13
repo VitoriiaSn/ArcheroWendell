fd06512e5b42a72e957c990b2f5d31436e3568ae
Vitória Evangelista Santos <20211170150037@ifrn.local>
2023-12-13 17:40:24 -0300

    atualização

--- jogo_wendell/Assets/Scripts/Player.cs
+++ jogo_wendell/Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Animator anim;
     public float speed;
     public int vida;
     
@@ -19,10 +20,16 @@ public class Player : MonoBehaviour
     {
         Move();
     }
-
+    
     private void Move()
     {
         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        transform.position = transform.position + movement * speed * Time.deltaTime;
+        
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.magnitude);
+
         transform.position = transform.position + movement * speed * Time.deltaTime;
     }
 }
