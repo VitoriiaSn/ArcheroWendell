bc7b4c8c83e49a567a578b31488bcbc51e2646d4
Vitória Evangelista Santos <20211170150037@ifrn.local>
2023-12-21 16:09:57 -0300

    movimentação e animação

--- jogo_wendell/Assets/Scripts/Player.cs
+++ jogo_wendell/Assets/Scripts/Player.cs
@@ -4,32 +4,31 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D playerRigidbody2D;
+    private Vector2 PlayerDirection;
     public Animator anim;
     public float speed;
     public int vida;
-    
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-    }
-    
-    private void Move()
-    {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
-        transform.position = transform.position + movement * speed * Time.deltaTime;
-        
-        anim.SetFloat("Horizontal", movement.x);
-        anim.SetFloat("Vertical", movement.y);
-        anim.SetFloat("Speed", movement.magnitude);
+        PlayerDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        transform.position = transform.position + movement * speed * Time.deltaTime;
+        // Atualizar parâmetros do Animator
+        anim.SetFloat("Horizontal", PlayerDirection.x);
+        anim.SetFloat("Vertical", PlayerDirection.y);
+        anim.SetFloat("Speed", PlayerDirection.magnitude);
+    }
+
+    void FixedUpdate()
+    {
+        playerRigidbody2D.MovePosition(playerRigidbody2D.position + PlayerDirection * speed * Time.fixedDeltaTime);
     }
 }
