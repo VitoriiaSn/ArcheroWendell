ddca6d28a9c1e7358a85f777c47aaba3b044d99a
Vitória Evangelista Santos <20211170150037@ifrn.local>
2023-12-27 17:38:12 -0300

    atualização

--- jogo_wendell/Assets/Scripts/Player.cs
+++ jogo_wendell/Assets/Scripts/Player.cs
@@ -1,35 +1,49 @@
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Mathematics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private Rigidbody2D playerRigidbody2D;
-    private Vector2 PlayerDirection;
-    public Animator anim;
-    public float speed;
-    public int vida;
-
-    void Start()
-    {
-        playerRigidbody2D = GetComponent<Rigidbody2D>();
-    }
-
+    public float rotationSpeed = 100f;
+    public float movementSpeed = 5f;
+    public GameObject tiroProjetil;
+    public Transform arma;
+    private bool disparo;
+    public float forcaDoDisparo;
+    private bool flipX = false;
     
     void Update()
     {
-        PlayerDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        
-        anim.SetFloat("Horizontal", PlayerDirection.x);
-        anim.SetFloat("Vertical", PlayerDirection.y);
-        anim.SetFloat("Speed", PlayerDirection.magnitude);
-    }
-    
+        RotatePlayer();
+        MovePlayer();
 
-    void FixedUpdate()
+        disparo = Input.GetKey(KeyCode.E);
+        Atirar();
+    }
+
+    private void Atirar()
     {
-        playerRigidbody2D.MovePosition(playerRigidbody2D.position + PlayerDirection * speed * Time.fixedDeltaTime);
+        if (disparo == true)
+        {
+            GameObject temp = Instantiate(tiroProjetil);
+            temp.transform.position = arma.position;
+            temp.GetComponent<Rigidbody2D>().velocity = new Vector2(forcaDoDisparo, 0);
+        }
+    }
+
+    void RotatePlayer()
+    {
+        float rotationInput = Input.GetAxis("Horizontal");
+        transform.Rotate(0, 0, -rotationInput * rotationSpeed * Time.deltaTime);
+    }
+
+    void MovePlayer()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(0, verticalInput * movementSpeed * Time.deltaTime, 0);
     }
 }
